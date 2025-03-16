e79d98ec262cf89fb0df3512b455dcac216a9ea5
cosin2077 <bermasunita6@gmail.com>
2025-03-16 11:15:23 +0800

    chore:

--- packages/frontend/src/router/index.ts
+++ packages/frontend/src/router/index.ts
@@ -1,13 +1,16 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import { createWebHistory, createRouter } from 'vue-router'
 
 import HomeView from '../views/Home.vue'
+import AboutView from '../views/About.vue'
+import NovelToAudio from '../views/NovelToAudio.vue'
 
 const routes = [
-  { path: '/', component: HomeView },
-  { path: '/about', component: HomeView },
+  { path: '/', component: NovelToAudio },
+  { path: '/novel-to-audio', component: NovelToAudio },
+  { path: '/about', component: AboutView },
 ]
 
 export const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes,
 })
