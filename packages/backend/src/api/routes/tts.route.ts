5f456c28267dd149fac29735e83564f793efacaf
cosin2077 <bermasunita6@gmail.com>
2025-03-17 14:42:11 +0800

    chore:

--- packages/backend/src/api/routes/tts.route.ts
+++ packages/backend/src/api/routes/tts.route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { generateAudio, downloadAudio, getVoices } from "../controllers/tts.controller";
+import { validateGenerate } from "../../schema/generate";
 
 const router = Router();
 
 router.get("/voices", getVoices);
-router.post("/generate", generateAudio);
+router.post("/generate", validateGenerate, generateAudio);
 router.get("/download/:file", downloadAudio);
 
 export default router;
