457efe7db9e41809059d8908cc8565a21b746b68
cosin2077 <bermasunita6@gmail.com>
2025-03-18 11:59:00 +0800

    chore:

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -5,13 +5,14 @@ import { logger } from "./utils/logger";
 import path from "path";
 import cors from 'cors';
 import { requestLoggerMiddleware } from "./middleware/info.middleware";
+import { AUDIO_DIR } from "./config";
 
 export function createApp(): Application {
   const app = express();
 
   app.use(cors());
   app.use(express.json());
-  app.use(express.static(path.join(__dirname, "..", 'audio')));
+  app.use(express.static(AUDIO_DIR));
 
   app.use(requestLoggerMiddleware);
   app.use("/api", ttsRoutes);
