58fa8cbf2683354aaff21f3691c061f7b063500b
cosin2077 <bermasunita6@gmail.com>
2025-03-19 15:34:43 +0800

    chore:

---
+++ scripts/run.sh
@@ -0,0 +1,9 @@
+docker run -d \
+  --restart unless-stopped \
+  --name easyvoice \
+  -p 3000:3000 \
+  -v audio:/app/audio \
+  -e OPENAI_BASE_URL=https://api.openai.com/v1 \
+  -e OPENAI_KEY=your_openai_key_here \
+  -e MODEL_NAME=gpt-4o-mini \
+  cosin2077/easyvoice:latest
